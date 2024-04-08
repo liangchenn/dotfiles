69d5914bb274b0279ff3e47fe81cd9bf13f45e8a
liangchenn <r08323022@ntu.edu.tw>
2024-04-08 14:25:55 +0800

    add neovim config

---
+++ nvim/lua/plugins/autocomplete.lua
@@ -0,0 +1,15 @@
+return {
+    -- nvim-cmp
+    {"hrsh7th/nvim-cmp"},
+    -- LSP
+    {"hrsh7th/cmp-nvim-lsp"},
+    -- snippet
+    {"hrsh7th/cmp-vsnip"},
+    {"saadparwaiz1/cmp_luasnip"},
+    {"hrsh7th/vim-vsnip"},
+    -- other complete
+    {"hrsh7th/cmp-path"},
+    {"hrsh7th/cmp-buffer"},
+    {"onsails/lspkind-nvim"},
+}
+  
