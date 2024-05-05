ed0c924d34992e3a70454f1508ffca2ee177ceb3
liangchenn <r08323022@ntu.edu.tw>
2024-05-06 02:44:40 +0800

    feat(keymap): setup keybindings
    - telescope dropdown
    - resizing
    - exit window

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -8,19 +8,23 @@ local map = require("helpers.keys").map
 map('i', 'jj', '<esc>')
 
 -- document operations
-map('i', 'js', '<cmd>update<cr><esc>', "Save document")
+-- map('i', 'js', '<cmd>update<cr><esc>', "Save document")
 
 -- Resize with arrows
-map("n", "<C-Up>", ":resize +2<CR>")
-map("n", "<C-Down>", ":resize -2<CR>")
-map("n", "<C-Left>", ":vertical resize +2<CR>")
-map("n", "<C-Right>", ":vertical resize -2<CR>")
+map("n", "<M-Up>", ":resize -2<CR>")
+map("n", "<M-Down>", ":resize +2<CR>")
+map("n", "<M-S-Left>", ":vertical resize -2<CR>")
+map("n", "<M-S-Right>", ":vertical resize +2<CR>")
 
 -- Clear after search
 map("n", "<leader>ur", "<cmd>nohl<cr>", "Clear highlights")
 
 -- Deleting buffers
 local buffers = require("helpers.buffers")
-map("n", "<leader>db", buffers.delete_this, "Current buffer")
-map("n", "<leader>do", buffers.delete_others, "Other buffers")
-map("n", "<leader>da", buffers.delete_all, "All buffers")
+map("n", "<leader>db", buffers.delete_this, "Delete Current buffer")
+map("n", "<leader>do", buffers.delete_others, "Delete Other buffers")
+map("n", "<leader>da", buffers.delete_all, "Delete All buffers")
+
+-- quit
+map("n", "<leader>qq", "<cmd>q<cr>", "quit current window")
+
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -26,7 +26,7 @@ return {
 			local map = require("helpers.keys").map
 			map("n", "<leader>ff", require("telescope.builtin").oldfiles, "Recently opened")
 			map("n", "<leader><space>", require("telescope.builtin").buffers, "Open buffers")
-			map("n", "<leader>/", function()
+			map("n", "<leader>,", function()
 				-- You can pass additional configuration to telescope to change theme, layout, etc.
 				require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 					winblend = 10,
