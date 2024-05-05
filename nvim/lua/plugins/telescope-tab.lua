cc228336dae58a2850ecd6c95e0ad9b4772363c8
liangchenn <r08323022@ntu.edu.tw>
2024-05-06 03:50:45 +0800

    feat(plugin): add telescope-tabs

---
+++ nvim/lua/plugins/telescope-tab.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        'LukasPietzschmann/telescope-tabs',
+        dependencies = { 'nvim-telescope/telescope.nvim' },
+        config = function()
+            require('telescope').load_extension 'telescope-tabs'
+            require('telescope-tabs').setup {
+                -- custom setups
+            }
+            -- custom keybindings
+            local map = require("helpers.keys").map
+            map("n", "<leader><TAB>", "<cmd> Telescope telescope-tabs list_tabs<cr>", "Toggle tab panel")
+        end,
+    },
+}
