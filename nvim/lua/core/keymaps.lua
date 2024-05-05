c5eacd96ea4c43051192726cf76ec07b2e853a62
liangchenn <r08323022@ntu.edu.tw>
2024-05-05 16:41:26 +0800

    feat(core): remove unused keybindings

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -8,7 +8,7 @@ local map = require("helpers.keys").map
 map('i', 'jj', '<esc>')
 
 -- document operations
-map('i', 'js', '<cmd>update<cr>', "Save document")
+map('i', 'js', '<cmd>update<cr><esc>', "Save document")
 
 -- Resize with arrows
 map("n", "<C-Up>", ":resize +2<CR>")
@@ -24,14 +24,3 @@ local buffers = require("helpers.buffers")
 map("n", "<leader>db", buffers.delete_this, "Current buffer")
 map("n", "<leader>do", buffers.delete_others, "Other buffers")
 map("n", "<leader>da", buffers.delete_all, "All buffers")
-
-
--- vim.keymap.set('i', 'jj', '<ESC>', { noremap = true, silent = true})
--- vim.keymap.set('n', '<leader>t', ':Neotree toggle<CR>', { noremap = true, silent = true })
--- vim.keymap.set('n', '<leader>f', ':Neotree<CR>', { noremap = true, silent = true })
--- vim.keymap.set('n', '<C-s>', ':update<CR>', { noremap = true, silent = true })
--- vim.keymap.set('i', '<C-s>', '<ESC>:update<CR>', { noremap = true, silent = true })
--- vim.keymap.set({'i', 'c'}, '<C-y>', '<A-.>', { noremap = true, silent = true})
--- vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true})
--- vim.keymap.set('t', 'jj', '<C-\\><C-n>', { noremap = true, silent = true})
-
