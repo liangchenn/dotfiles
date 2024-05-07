91093e506267831070ebbab8122f516ce27883a1
liangchenn <r08323022@ntu.edu.tw>
2024-05-07 14:29:33 +0800

    feat(plugin): change toggle term keymap to avoid mistyping

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
         'akinsho/toggleterm.nvim', version = "*",
         config = function()
             require("toggleterm").setup({
-                open_mapping = [[<leader>t]],
+                open_mapping = [[<leader>\]],
                 size = 20,
                 direction = 'float',
                 close_on_exit = true,
